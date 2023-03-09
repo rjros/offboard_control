b28a2ab512d6e59fdab79d25dafc27b4958cac6a
rjros <81947104+rjros@users.noreply.github.com>
2023-03-09 17:48:11 +0900

    Hover value for s500 drone

--- offboard/scripts/attitude_setpoints.py
+++ offboard/scripts/attitude_setpoints.py
@@ -24,7 +24,7 @@ from mavros_msgs.srv import *
 P_roll=0.0
 I_roll=0.0
 D_roll=0
-max_roll=1.5
+max_roll=2
 #Variable for the PID pitch 
 P_pitch=0.0
 I_pitch=0.0
@@ -32,11 +32,11 @@ D_pitch=0
 max_pitch=2
 
 #Variable for the PID thrust 
-P_thrust=0.1
+P_thrust=0.0
 I_thrust=0.0
 D_thrust=0.0
-max_thrust=0.01
-min_thrust=-0.001 #system just needs to lower from the hover state 
+max_thrust=0.1
+min_thrust=-0.1 #system just needs to lower from the hover state 
 
 #Debugging, testing 1 point
 target_pose=np.array([0.0,0.0,0.0])
@@ -100,15 +100,25 @@ class PID:
 class Control:
     def __init__(self):
         #Channels RC
-        self.mode_select_channel=9#Choose between the attitude controller , the position setpoint controller and position hold 
-        self.mode_select=0
+        self.roll_select_channel=1
+        self.pitch_select_channel=2#reverse channel in code
+        self.altitude_select_channel=3
+        self.yaw_select_channel=4
+        self.mode_select_channel=9
+        #Use the RC sticks to control the drone for debugging or semi-autonomous work
+        self.mode_select=0   #Choose between the attitude controller ,the position setpoint controller and position hold (not implemented)
+        self.roll_channel=0  #1082-1921
+        self.pitch_channel=0 #1919-1079
+        self.yaw_channel=0  #1082-1921
+        self.altitude_channel=0 #1029-1934 
         #Commands to be sent in the attitude controller, in degrees
         #limit the roll and pitch 
         self.roll=0
         self.pitch=0
         self.yaw=0
-        self.thrust=0.0 #mantain height
-        self.hover=0.67 #for simulation confirm with actual drone 
+        self.thrust=0.0 #maintain height
+        self.hover=0.55 #for simulation confirm with actual drone 
+        self.angle=5 #maximum tilt angle
         self.quatenion_rot=[0.0,0.0,0.0,0.0]#maybe numpy array problem 
 
         #State flags 
@@ -132,11 +142,22 @@ class Control:
         self.pos_x=0.0
         self.pos_y=0.0
         self.pos_z=0.0
-
+    
+    def map(self,OldValue, OldMin, OldMax, NewMin, NewMax):
+        return (((OldValue - OldMin) * (NewMax - NewMin)) / (OldMax - OldMin)) + NewMin
+    
     #Use to change between control modes
     def rc_callback(self,msg):
         # 3 Pos switches 1094-1514-1934
         self.RCin=msg.channels
+
+        self.roll_channel=self.map(self.RCin[self.roll_select_channel-1],1082,1921,-self.angle,self.angle)
+        self.pitch_channel=self.map(self.RCin[self.pitch_select_channel-1],1079,1919,-self.angle,self.angle)
+        self.yaw_channel=self.map(self.RCin[self.yaw_select_channel-1],1082,1921,-self.angle,self.angle)
+        ##small values are enough to change altitude##
+        self.altitude_channel=self.map(self.RCin[self.altitude_select_channel-1],1082,1921,-0.1,0.1)
+        # print(self.roll_channel,self.pitch_channel,self.yaw_channel,self.altitude_channel)
+
         self.mode_select=self.RCin[self.mode_select_channel-1]         
         if (self.mode_select_channel<=1514):    
             self.mode_position=True
@@ -146,19 +167,28 @@ class Control:
             self.mode_attitude=True 
 
     def commands(self,roll_cmd,pitch_cmd,yaw_cmd,thrust_cmd,stamp):
+        
         #update stamp
         self.attitude_setpoints.header.stamp=stamp
-        #Attitude commands, convert to radians
-        # print(roll_cmd,pitch_cmd,yaw_cmd)
-        self.roll=roll_cmd*pi/180
-        self.pitch=pitch_cmd*pi/180
-        self.yaw=yaw_cmd*pi/180
-        #Fixed 
+        
+        ##Attitude commands, convert to radians##
+        # self.roll=roll_cmd*pi/180
+        # self.pitch=pitch_cmd*pi/180
+        # self.yaw=yaw_cmd*pi/180
+        
+        ##Using values from the stick##
+        self.roll=self.roll_channel*pi/180
+        self.pitch=-self.pitch_channel*pi/180
+        self.yaw= 90*pi/180#+self.yaw_channel*pi/180
+        # print(self.roll,self.pitch,self.yaw)
+        ##Fixed##
         self.attitude_setpoints.body_rate.x=0
         self.attitude_setpoints.body_rate.y=0
         self.attitude_setpoints.body_rate.z=0
-        #Thrust command [0,1] test + hover state ~0.67
-        self.attitude_setpoints.thrust=self.hover +thrust_cmd
+        ##Thrust command [0,1] test + hover state ~0.67##
+        # self.attitude_setpoints.thrust=self.hover + thrust_cmd
+        self.attitude_setpoints.thrust=self.hover + self.altitude_channel
+
 
     def send_attitude(self):
         self.quaternion_rot=quaternion_from_euler(self.roll,self.pitch,self.yaw)
@@ -218,7 +248,7 @@ def main():
     thrust_cmd.reset_values()
 
     pub = rospy.Publisher("mavros/setpoint_raw/attitude",AttitudeTarget,queue_size=1)
-    pub_plot=rospy.Publisher("plot_data",Vector3,queue_size=1)
+    #pub_plot=rospy.Publisher("plot_data",Vector3,queue_size=1)
     rospy.Subscriber('/mavros/rc/in', RCIn, control.rc_callback, queue_size=1)
     rospy.Subscriber('mavros/local_position/pose', PoseStamped,control.pose_callback,queue_size=1)
     rospy.Subscriber('mavros/state',State,control.state_callback,queue_size=1)
@@ -229,7 +259,7 @@ def main():
     print('Position received')
     target_pose[0]=control.pos_x #+0.5
     target_pose[1]=control.pos_y #+0.5
-    target_pose[2]=0.6 #mantain height depends motor
+    target_pose[2]=control.pos_z #maintain height depends motor
 
     rate = rospy.Rate(100) # 
     while not rospy.is_shutdown():
@@ -258,14 +288,14 @@ def main():
         stamp=rospy.Time.now()  # stamp should update
 
         #####################################################################################
-        #####################3Full Controller############
+        #####################3Full Controller################################################
         # control.commands(roll_cmd.output,pitch_cmd.output,yaw_cmd,thrust_cmd.output,stamp)
         #####################################################################################
+        
         #For testing a fixed height and no rotation
         control.commands(roll_test,pitch_test,yaw_test,thrust_cmd.output,stamp)
-
         pub.publish(control.send_attitude())
-        pub_plot.publish(control.plot_data(target_pose[2],control.pos_z,control.attitude_setpoints.thrust))
+        #pub_plot.publish(control.plot_data(target_pose[2],control.pos_z,control.attitude_setpoints.thrust))
         #print('Pterm',thrust_cmd.PTerm,'P output',thrust_cmd.output,'CMD',control.attitude_setpoints.thrust)
         rate.sleep()
             
